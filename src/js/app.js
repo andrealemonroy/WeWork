7659340ff358ea8ff5576a11f468f08bbf4a5f16
Grecia <gutierrezanicamalucero@gmail.com>
2018-08-07 09:33:42 -0500

    subiendo registro de usuario yvisitante

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC4Y01sM0a675WxyORJyYeaozIcr5UViXM",
+    authDomain: "wework-peru.firebaseapp.com",
+    databaseURL: "https://wework-peru.firebaseio.com",
+    projectId: "wework-peru",
+    storageBucket: "wework-peru.appspot.com",
+    messagingSenderId: "774312303165"
+  };
+  firebase.initializeApp(config);
+// *************************************
+
+
+
+
---
+++ src/js/main.js
@@ -0,0 +1,72 @@
+// Conectanto ID del HTML con el JS
+const registerVisitatorButton = document.getElementById('register-Visitator-Button');
+const registerAdministratorButton = document.getElementById('register-Administrator-Button');
+const registerVisited = document.getElementById('register-visited');
+const registerAdmin = document.getElementById('register-admin');
+const registerVisitor = document.getElementById('register-visitor');
+
+
+// Botones al ingresar
+const visitorButton = document.getElementById('visitor-Button');
+const adminButton = document.getElementById('admin-Button');
+const registerChoose = document.getElementById('register-choose');
+ 
+
+
+  // registerAdmin.classList.replace('block', 'none');
+  // registerVisitor.classList.replace('block', 'none');
+
+
+
+// Boton como Visitante
+visitorButton.addEventListener('click', () => {
+  registerVisitor.classList.remove('none');
+  registerVisitor.classList.add('block');
+  registerAdmin.classList.replace('block', 'none');
+})
+
+// boton como Administrador
+adminButton.addEventListener('click', () => {
+  registerAdmin.classList.remove('none');
+  registerAdmin.classList.add('block');
+  registerVisitor.classList.replace('block', 'none');
+})
+
+
+// Registro del visitante
+registerVisitatorButton.addEventListener('click', () => {
+  const name = document.getElementById('name').value;
+  const celular = document.getElementById('celular').value;
+  const aQuienVisita = document.getElementById('a-quien-visita').value;
+  const company = document.getElementById('company').value;
+
+  console.log(name.value);
+  console.log(company);
+  const data = firebase.database().ref().child('visitator');
+  data.push({
+    name: name,
+    celular: celular,
+    aQuienVisita: aQuienVisita,
+    company: company,
+  })
+  // console.log(name);
+  alert("Estimado visitante su Registro fue exitoso")
+})
+
+// Registro del Administrador
+registerAdministratorButton.addEventListener('click', () => {
+  const nameAdmin = document.getElementById('name-admin').value;
+  const celularAdmin = document.getElementById('celular-admin').value;
+  const emailAdmin = document.getElementById('email-admin').value;
+  const passwordAdmin = document.getElementById('password-admin').value;
+  console.log(nameAdmin, celularAdmin, emailAdmin, passwordAdmin);
+  const data = firebase.database().ref().child('administrator');
+  data.push({
+    nameAdmin: nameAdmin,
+    celularAdmin: celularAdmin,
+    emailAdmin: emailAdmin,
+    password: passwordAdmin
+  })
+  alert("Estimado colaborador su Registro fue exitoso")
+})
+
